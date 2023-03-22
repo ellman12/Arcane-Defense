b235facd711cbb205483696dfeab6bbf4cdc2083
Elliott <ellduc4@gmail.com>
2023-03-22 15:32:28 -0500

    Add Singleton<T> class

---
+++ Arcane-Defense/Assets/Scripts/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Object
+{
+	public static T I { get; private set; }
+
+	protected void Awake()
+	{
+		if (I == null)
+		{
+			I = FindObjectOfType<T>();
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Debug.LogWarning($"{gameObject.name} destroyed.", this);
+			Destroy(gameObject);
+		}
+	}
+}
