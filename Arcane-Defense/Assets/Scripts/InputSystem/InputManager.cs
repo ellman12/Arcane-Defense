a8c3cf14145587e3295409bc81a46e5f9b53cfa2
Elliott DuCharme <ellduc4@gmail.com>
2023-03-22 16:32:00 -0500

    Create PlayerInput and InputManager

---
+++ Arcane-Defense/Assets/Scripts/InputSystem/InputManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InputSystem
+{
+	public class InputManager : Singleton<InputManager>
+	{
+		public PlayerInput PlayerInput { get; private set; }
+		
+		private new void Awake()
+		{
+			PlayerInput = new PlayerInput();
+			PlayerInput.Enable();
+			base.Awake();
+		}
+	}
+}
