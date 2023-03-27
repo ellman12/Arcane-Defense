0cba81a3e829aacd825a269c8152e9e53379face
Elliott DuCharme <ellduc4@gmail.com>
2023-03-27 16:51:23 -0500

    SwordEnemy will now move towards the player

---
+++ Arcane-Defense/Assets/Scripts/Enemies/SwordEnemy.cs
@@ -0,0 +1,25 @@
+﻿using InputSystem;
+using UnityEngine;
+
+namespace Enemies
+{
+	public class SwordEnemy : MonoBehaviour
+	{
+		[SerializeField] private float moveSpeed, stoppingDistance;
+
+		private Transform player, playerHouse;
+
+		private void Start()
+		{
+			player = PlayerMovement.I.transform;
+		}
+
+		private void Update()
+		{
+			Vector2 direction = player.position - transform.position;
+
+			if (direction.magnitude > stoppingDistance)
+				transform.Translate(direction.normalized * (moveSpeed * Time.deltaTime));
+		}
+	}
+}
