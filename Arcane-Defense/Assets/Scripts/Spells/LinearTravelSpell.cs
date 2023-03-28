1c8774af372b9c58dd822f69ad6db15378a29191
Elliott DuCharme <ellduc4@gmail.com>
2023-03-28 11:05:03 -0500

    Create LinearTravelSpell.cs

---
+++ Arcane-Defense/Assets/Scripts/Spells/LinearTravelSpell.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Spells
+{
+	public class LinearTravelSpell : MonoBehaviour
+	{
+		[SerializeField] private float speed;
+		private Vector3 worldPOS, direction;
+
+		private void Awake()
+		{
+			Ray ray = Camera.main!.ScreenPointToRay(Input.mousePosition);
+			worldPOS = ray.GetPoint(10);
+			worldPOS.z = 0;
+			direction = worldPOS.normalized;
+		}
+
+		private void Update()
+		{
+			transform.position += direction * (speed * Time.deltaTime);
+		}
+	}
+}
