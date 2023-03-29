4f7bfee1a16d9ed2d3946594a278a90a463e2873
Elliott <ellduc4@gmail.com>
2023-03-28 22:45:49 -0500

    Create Water Shield prefab and script

---
+++ Arcane-Defense/Assets/Scripts/Spells/WaterShield.cs
@@ -0,0 +1,13 @@
+﻿using InputSystem;
+
+namespace Spells
+{
+	public class WaterShield : Spell
+	{
+		private void Start()
+		{
+			transform.position = PlayerMovement.I.transform.position;
+			transform.parent = PlayerMovement.I.transform;
+		}
+	}
+}
