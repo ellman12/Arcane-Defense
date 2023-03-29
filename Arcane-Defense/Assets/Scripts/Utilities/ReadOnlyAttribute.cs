09708cce452d70d451303cc945a5ead0cb981859
Elliott <ellduc4@gmail.com>
2023-03-29 00:02:13 -0500

    Add Utilities folder and ReadOnlyAttribute

---
+++ Arcane-Defense/Assets/Scripts/Utilities/ReadOnlyAttribute.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace Utilities
+{
+	[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+	internal class ReadOnlyDrawer : PropertyDrawer
+	{
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			return EditorGUI.GetPropertyHeight(property, label, true);
+		}
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			GUI.enabled = false;
+			EditorGUI.PropertyField(position, property, label, true);
+			GUI.enabled = true;
+		}
+	}
+
+	///Displays a value in the Inspector that can't be modified.
+	public class ReadOnlyAttribute : PropertyAttribute
+	{
+	}
+}
+#endif
