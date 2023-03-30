18a589747963517fcf1d17e1c4f51c1a5af5255e
Elliott DuCharme <ellduc4@gmail.com>
2023-03-30 11:16:32 -0500

    Player health and cooldown add

---
+++ Arcane-Defense/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using Enemies;
+using UnityEngine;
+using Utilities;
+
+namespace Player
+{
+	public class PlayerHealth : Singleton<PlayerHealth>
+	{
+		[SerializeField] private float invincibilityDuration, invincibilityDeltaTime;
+		
+		[SerializeField, ReadOnly] private int health, maxHealth;
+		[SerializeField, ReadOnly] private bool invincible;
+
+		private int Health
+		{
+			get => health;
+			set
+			{
+				health = value;
+				if (health <= 0)
+					Destroy(gameObject);
+			}
+		}
+
+		private int MaxHealth
+		{
+			get => maxHealth;
+			set
+			{
+				maxHealth = value;
+			}
+		}
+
+		private void Start()
+		{
+			Health = 100;
+		}
+
+		private void OnTriggerStay2D(Collider2D col)
+		{
+			if (col.CompareTag("Enemy"))
+				LoseHealth(col.GetComponent<Enemy>().ContactDamage);
+		}
+		
+		private void LoseHealth(int amount)
+		{
+			if (invincible) return;
+
+			Health -= amount;
+
+			StartCoroutine(BecomeTemporarilyInvincible());
+		}
+
+		private IEnumerator BecomeTemporarilyInvincible()
+		{
+			invincible = true;
+
+			for (float i = 0; i < invincibilityDuration; i += invincibilityDeltaTime)
+			{
+				transform.localScale = transform.localScale == Vector3.one ? Vector3.zero : Vector3.one;
+				yield return new WaitForSeconds(invincibilityDeltaTime);
+			}
+
+			invincible = false;
+			transform.localScale = Vector3.one;
+		}
+	}
+}
