ab12804de10688a205dffc89ca355e25f9ab64be
Elliott <ellduc4@gmail.com>
2023-03-31 17:56:56 -0500

    Sword Enemy will now chase player if close enough, otherwise will chase the house

--- Arcane-Defense/Assets/Scripts/Enemies/SwordEnemy.cs
+++ Arcane-Defense/Assets/Scripts/Enemies/SwordEnemy.cs
@@ -1,25 +1,38 @@
 ﻿using InputSystem;
+using Player;
 using UnityEngine;
 
 namespace Enemies
 {
 	public class SwordEnemy : Enemy
 	{
-		[SerializeField] private float moveSpeed, stoppingDistance;
+		[SerializeField] private float moveSpeed, playerStopDist, houseStopDist, playerChaseDistance, houseChaseDistance;
 
-		private Transform player, playerHouse;
+		private Transform player, house;
+		private bool attackingHouse;
 
 		private void Start()
 		{
 			player = PlayerMovement.I.transform;
+			house = PlayerHouse.I.transform;
 		}
 
 		private void Update()
 		{
-			Vector2 direction = player.position - transform.position;
+			if (attackingHouse) return;
 
-			if (direction.magnitude > stoppingDistance)
-				transform.Translate(direction.normalized * (moveSpeed * Time.deltaTime));
+			Vector2 playerDirection = player.position - transform.position;
+			Vector2 houseDirection = house.position - transform.position;
+			
+			if (playerDirection.magnitude < playerChaseDistance && playerDirection.magnitude > playerStopDist)
+				transform.Translate(playerDirection.normalized * (moveSpeed * Time.deltaTime));
+			else if (houseDirection.magnitude < houseChaseDistance)
+			{
+				if (houseDirection.magnitude < houseStopDist)
+					attackingHouse = true;
+				else
+					transform.Translate(houseDirection.normalized * (moveSpeed * Time.deltaTime));
+			}
 		}
 	}
 }
