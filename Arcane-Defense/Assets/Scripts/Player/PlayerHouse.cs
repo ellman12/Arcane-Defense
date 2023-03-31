f7a1ebe276fa53d09b33963d3a31187475f2ab48
Elliott <ellduc4@gmail.com>
2023-03-31 17:13:37 -0500

    Add house with a health bar to scene. Non-functional

---
+++ Arcane-Defense/Assets/Scripts/Player/PlayerHouse.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Utilities;
+
+namespace Player
+{
+	public class PlayerHouse : Singleton<PlayerHouse>
+	{
+		[SerializeField, ReadOnly] private int health, maxHealth;
+		[SerializeField] private StatBar healthBar;
+
+		private int Health
+		{
+			get => health;
+			set
+			{
+				health = value;
+				healthBar.SetValue(health);
+				if (health <= 0)
+					Destroy(gameObject);
+			}
+		}
+
+		private int MaxHealth
+		{
+			get => maxHealth;
+			set
+			{
+				maxHealth = value;
+				healthBar.SetMaxValue(maxHealth);
+			}
+		}
+
+		private new void Awake()
+		{
+			MaxHealth = Health = 100;
+			base.Awake();
+		}
+	}
+}
