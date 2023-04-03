56f2ae271774f701014fac31d2c11af604edd7ff
Elliott <ellduc4@gmail.com>
2023-04-03 00:33:02 -0500

    You can now summon Turrets at the mouse cursor

--- Arcane-Defense/Assets/Scripts/Spells/Turret.cs
+++ Arcane-Defense/Assets/Scripts/Spells/Turret.cs
@@ -19,6 +19,11 @@ namespace Spells
 
 		private void Start()
 		{
+			Vector3 mousePosition = Input.mousePosition;
+			mousePosition.z = -Camera.main.transform.position.z;
+			Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+			transform.position = worldPosition;
+			
 			StartCoroutine(Target());
 			StartCoroutine(AttackTarget());
 		}
@@ -49,6 +54,11 @@ namespace Spells
 			while (true)
 			{
 				currentTarget = GetClosestTarget();
+				if (currentTarget == null)
+				{
+					yield return new WaitForSeconds(enemyInRangeCheckCooldown);
+					continue;
+				}
 				
 				Vector3 direction = currentTarget.position - transform.position;
 				float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
@@ -62,6 +72,12 @@ namespace Spells
 		{
 			while (true)
 			{
+				if (currentTarget == null)
+				{
+					yield return new WaitForSeconds(enemyInRangeCheckCooldown);
+					continue;
+				}
+				
 				var newSpell = Instantiate(spell, transform.position, Quaternion.identity) as LinearTravelSpell ?? throw new NullReferenceException();
 				newSpell.target = currentTarget;
 				newSpell.speed *= 2;
