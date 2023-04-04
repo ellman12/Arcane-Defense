00c9489200150e8533840d459252e49c8d3744e6
Elliott DuCharme <ellduc4@gmail.com>
2023-04-04 12:08:19 -0500

    Good start on Chain Lightning

---
+++ Arcane-Defense/Assets/Scripts/Spells/ChainLightning.cs
@@ -0,0 +1,36 @@
+﻿using Enemies;
+using InputSystem;
+using UnityEngine;
+
+namespace Spells
+{
+	public class ChainLightning : Spell
+	{
+		[SerializeField] private LineRenderer lineRenderer;
+
+		[SerializeField] private float xDeltaMin, xDeltaMax, yDeltaMin, yDeltaMax;
+
+		[SerializeField] private int posCount;
+
+		[SerializeField] private Enemy target;
+
+		private void Start()
+		{
+			lineRenderer.positionCount = posCount;
+		}
+
+		private void FixedUpdate()
+		{
+			for (int i = 1; i < posCount - 1; i++)
+			{
+				Vector2 position = Vector2.Lerp(PlayerMovement.I.transform.position, target.transform.position, (float) i / posCount);
+				position.x += Random.Range(xDeltaMin, xDeltaMax);
+				position.y += Random.Range(yDeltaMin, yDeltaMax);
+				lineRenderer.SetPosition(i, position);
+			}
+
+			lineRenderer.SetPosition(0, PlayerMovement.I.transform.position);
+			lineRenderer.SetPosition(posCount - 1, target.transform.position);
+		}
+	}
+}
