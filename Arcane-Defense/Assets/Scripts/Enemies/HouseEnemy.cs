e7a399d953c17e867c4a2b9301cd8a92356e1f4e
Elliott <ellduc4@gmail.com>
2023-04-06 22:53:27 -0500

    Create House Enemy

---
+++ Arcane-Defense/Assets/Scripts/Enemies/HouseEnemy.cs
@@ -0,0 +1,38 @@
+﻿using InputSystem;
+using Player;
+using UnityEngine;
+
+namespace Enemies
+{
+	public class HouseEnemy : Enemy
+	{
+		[SerializeField] private float houseContactDamage, moveSpeed, playerStopDist, houseStopDist, playerChaseDistance, houseChaseDistance;
+
+		private Transform player, house;
+		private bool attackingHouse;
+
+		private void Start()
+		{
+			player = PlayerMovement.I.transform;
+			house = PlayerHouse.I.transform;
+		}
+
+		private void Update()
+		{
+			if (attackingHouse || !canMove || player == null || house == null) return;
+		
+			Vector2 playerDirection = player.position - transform.position;
+			Vector2 houseDirection = house.position - transform.position;
+			
+			if (playerDirection.magnitude < playerChaseDistance && playerDirection.magnitude > playerStopDist)
+				transform.Translate(playerDirection.normalized * (moveSpeed * Time.deltaTime));
+			else if (houseDirection.magnitude < houseChaseDistance)
+			{
+				if (houseDirection.magnitude < houseStopDist)
+					attackingHouse = true;
+				else
+					transform.Translate(houseDirection.normalized * (moveSpeed * Time.deltaTime));
+			}
+		}
+	}
+}
