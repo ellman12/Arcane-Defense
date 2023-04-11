cb922b5afeb1fe6812939fdc3ecb7df9abad4b14
Elliott DuCharme <ellduc4@gmail.com>
2023-04-11 16:44:43 -0500

    Create Scripts/UI folder

---
+++ Arcane-Defense/Assets/Scripts/UI/TitleScreen.cs
@@ -0,0 +1,22 @@
+﻿using InputSystem;
+using UnityEngine.SceneManagement;
+using CBC = UnityEngine.InputSystem.InputAction.CallbackContext;
+
+namespace UI
+{
+	public class TitleScreen : Singleton<TitleScreen>
+	{
+		private new void Awake()
+		{
+			base.Awake();
+			InputManager.I.PlayerInput.TitleScreen.StartGame.performed += LoadGame;
+		}
+
+		private void LoadGame(CBC _)
+		{
+			Destroy(InputManager.I);
+			Destroy(gameObject);
+			SceneManager.LoadScene("Level");
+		}
+	}
+}
