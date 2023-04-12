62c0c351ca568a8249b23b69d888f869c7782f44
Elliott <ellduc4@gmail.com>
2023-04-11 23:03:11 -0500

    Spells can be unlocked (I think) but they can't be added to the spells menu yet

--- Arcane-Defense/Assets/Scripts/UI/SpellsMenu.cs
+++ Arcane-Defense/Assets/Scripts/UI/SpellsMenu.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using InputSystem;
+using Spells;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,6 +11,12 @@ namespace UI
 	public class SpellsMenu : Singleton<SpellsMenu>
 	{
 		public List<SpellsMenuSlot> spellSlots;
+		
+		[SerializeField] private List<SpellInfo> unlockableSpells;
+
+		[SerializeField] private List<int> spellRoundUnlock;
+
+		private readonly List<SpellInfo> availableSpells = new();
 
 		private new void Awake()
 		{
@@ -15,10 +24,27 @@ namespace UI
 			gameObject.SetActive(false);
 			InputManager.I.PlayerInput.SpellsMenu.Toggle.performed += Toggle;
 
-			void Toggle(InputAction.CallbackContext callbackContext)
+			GameManager.I.RoundAdvance += CheckForUnlockedSpells;
+
+			if (unlockableSpells.Count != spellRoundUnlock.Count) throw new ArgumentOutOfRangeException();
+		}
+
+		private void Toggle(InputAction.CallbackContext callbackContext)
+		{
+			gameObject.SetActive(!gameObject.activeSelf);
+			Time.timeScale = gameObject.activeSelf ? 0 : 1;
+		}
+
+		private void CheckForUnlockedSpells(object sender, EventArgs eventArgs)
+		{
+			for (int i = 0; i < unlockableSpells.ToArray().Length; i++)
 			{
-				gameObject.SetActive(!gameObject.activeSelf);
-				Time.timeScale = gameObject.activeSelf ? 0 : 1;
+				if (spellRoundUnlock[i] <= GameManager.I.RoundNumber && spellSlots.Count(s => s.spellInfo != null && s.spellInfo.name == unlockableSpells[i].name) == 0)
+				{
+					availableSpells.Add(unlockableSpells.First());
+					unlockableSpells.RemoveAt(0);
+					spellRoundUnlock.RemoveAt(0);
+				}
 			}
 		}
 	}
