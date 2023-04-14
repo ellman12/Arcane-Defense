ddb2a89cc232337ddde18f670f70a723b8af32bb
Elliott <ellduc4@gmail.com>
2023-04-13 20:06:04 -0500

    Create Infinite Mana Power Up

---
+++ Arcane-Defense/Assets/Scripts/PowerUps/InfiniteMana.cs
@@ -0,0 +1,13 @@
+﻿using Player;
+
+namespace PowerUps
+{
+	public class InfiniteMana : PowerUp
+	{
+		private void Update()
+		{
+			if (!active) return;
+			PlayerMana.I.Mana = PlayerMana.I.MaxMana;
+		}
+	}
+}
