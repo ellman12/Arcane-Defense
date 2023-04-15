c8814691cec83abf1cc1b06f38e9ae24c2ec2b97
Elliott <ellduc4@gmail.com>
2023-04-15 10:59:06 -0500

    Add AudioManager and SoundClip SO

---
+++ Arcane-Defense/Assets/Scripts/Audio/Audio.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+	[CreateAssetMenu(fileName = "Audio", menuName = "Audio", order = 0)]
+	public class Audio : ScriptableObject
+	{
+		public AudioClip audioClip;
+		[Range(0, 1)] public float volumeOffset;
+	}
+}
---
+++ Arcane-Defense/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+	public class AudioManager : Singleton<AudioManager>
+	{
+		[SerializeField] private AudioSource audioSource;
+		[SerializeField, Range(0, 1)] private float masterVolume;
+
+		public void PlaySound(Audio a)
+		{
+			// float volumeLevel = masterVolume + a.volumeOffset;
+			// aSource.PlayOneShot(a.aClip, Mathf.Clamp(volumeLevel, 0, masterVolume));
+			
+			audioSource.PlayOneShot(a.audioClip, masterVolume);
+		}
+	}
+}
