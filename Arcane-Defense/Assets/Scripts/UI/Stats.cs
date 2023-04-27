31a2573032de8bd83a6ef394778d6c2aafd70c1a
Elliott <ellduc4@gmail.com>
2023-04-27 10:42:21 -0500

    Stats doesn't display kills for an enemy if the amount of kills is 0

--- Arcane-Defense/Assets/Scripts/UI/Stats.cs
+++ Arcane-Defense/Assets/Scripts/UI/Stats.cs
@@ -11,6 +11,20 @@ namespace UI
 
 		private void Start() => text.text = "";
 
-		public void UpdateStats() => text.text = $"Kills\nSword: {swordEnemyKills}\nMage: {mageEnemyKills}\nHouse: {houseEnemyKills}\nShrek: {shrekKills}\nShadow: {shadowShrekKills}";
+		public void UpdateStats()
+		{
+			text.text = "Kills\n";
+
+			if (swordEnemyKills > 0)
+				text.text += $"Sword: {swordEnemyKills}\n";
+			if (mageEnemyKills > 0)
+				text.text += $"Mage: {mageEnemyKills}\n";
+			if (houseEnemyKills > 0)
+				text.text += $"House: {houseEnemyKills}\n";
+			if (shrekKills > 0)
+				text.text += $"Shrek: {shrekKills}\n";
+			if (shadowShrekKills > 0)
+				text.text += $"Shadow: {shadowShrekKills}\n";
+		}
 	}
 }
